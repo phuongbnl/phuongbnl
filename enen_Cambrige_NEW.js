883f1efad59eb03138563729a8d4eb52b1100e3b
phuongbnl <139679631+phuongbnl@users.noreply.github.com>
2023-07-16 18:45:03 +0700

    Update enen_Cambrige_NEW.js

--- enen_Cambrige_NEW.js
+++ enen_Cambrige_NEW.js
@@ -37,7 +37,7 @@ class enen_Cambridge {
         }
 
         let base = 'https://dictionary.cambridge.org/search/english/direct/?q=';
-        let url = base + encodeURIComponent(word);
+      
        
         let doc = '';
         try {
@@ -52,7 +52,7 @@ class enen_Cambridge {
         for (const entry of entries) {
             let definitions = [];
             let audios = [];
-            let extrainfo = = T(entry.querySelector('.headword'));
+            let url = [];
             let expression = T(entry.querySelector('.headword'));
             let reading = '';
             let readings = entry.querySelectorAll('.pron .ipa');
@@ -66,8 +66,8 @@ class enen_Cambridge {
             audios[0] = entry.querySelector(".uk.dpron-i source");
             audios[0] = audios[0] ? 'https://dictionary.cambridge.org' + audios[0].getAttribute('src') : '';
             //audios[0] = audios[0].replace('https', 'http');
-            audios[1] = entry.querySelector(".us.dpron-i source");
-            audios[1] = audios[1] ? 'https://dictionary.cambridge.org' + audios[1].getAttribute('src') : '';
+            url[0] = entry.querySelector(".us.dpron-i source");
+            url[0] = url[0] ? 'https://dictionary.cambridge.org' + url[0].getAttribute('src') : '';
             //audios[1] = audios[1].replace('https', 'http');
 
             let sensbodys = entry.querySelectorAll('.sense-body') || [];
@@ -117,7 +117,6 @@ class enen_Cambridge {
                 css,
                 expression,
                 reading,
-                extrainfo,
                 definitions,
                 audios
             });
